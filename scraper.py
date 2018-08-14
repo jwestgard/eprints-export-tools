49e189805cf723a68d28ddfaace30d55023f47ce
jwestgard <jwestgard@mac.com>
2018-08-14 02:34:34 -0400

    revise scraper

--- scraper.py
+++ scraper.py
@@ -1,43 +1,53 @@
 #!/usr/bin/env python3
 
-import requests
 import csv
+import os
+import requests
+import sys
 
-URI_PATTERN = ''' http://health-equity.lib.umd.edu/cgi/export/{0}/DC/minorityhealth-archive-{0}.txt'''
+URI_PATTERN = '''http://health-equity.lib.umd.edu/cgi/export/{0}/DC/minorityhealth-archive-{0}.txt'''
+OUTPUT_DIR = sys.argv[2]
 
-class DublinCore:
-    def __init__(self, uri):
-        response = requests.get(uri)
-        self.body = response.text
+def fetch_dc_metadata(filepath, uri):
+    print('Fetching {0}...'.format(uri))
+    response = requests.get(uri)
+    body = response.text
+    with open(filepath, 'w') as outfile:
+        outfile.write(body)
 
-    def parse_response_body(self):
-        lines = self.body.split('\n')
-        for line in lines:
+def read_dc_metadata(filepath, uri, id):
+    print('Reading {0}...'.format(filepath))
+    result = {'id':[str(id)], 'dc_uri':[uri]}
+    with open(filepath, 'r') as infile:
+        body = infile.read()
+        for line in body.split('\n'):
             if line == '':
                 continue
             (key, value) = tuple(line.split(': ', 1))
-            if hasattr(self, key):
-                current = getattr(self, key)
-                if type(current) is list:
-                    newval = current.append(value)
-                else:
-                    newval = [current, value]
-                setattr(self, key, newval)
-            else:
-                setattr(self, key, value)
-        
-for x in range(1000, 1050):
-    uri = URI_PATTERN.format(x)
-    response = requests.head(uri)
-    if response.status_code == 200:
-        dc_metadata = DublinCore(uri)
-        dc_metadata.parse_response_body()
-        for k, v in dc_metadata.__dict__.items():
-            if k == 'body':
-                continue
-            else:
-                print(k, v)
+            result.setdefault(key, []).append(value)
+    return result
 
+eprints = []
+existing_files = [os.path.join(OUTPUT_DIR, f) for f in os.listdir(OUTPUT_DIR)]
+print(existing_files)
+for id in range(5000):
+    print('Processing Eprint id {0}...'.format(id))
+    filepath = os.path.join(OUTPUT_DIR, '{0}.txt'.format(id))
+    uri = URI_PATTERN.format(id)
+    if filepath not in existing_files:
+        response = requests.head(uri)
+        if response.status_code == 200:
+            fetch_dc_metadata(filepath, uri)
+        else:
+            continue
+    eprints.append(read_dc_metadata(filepath, uri, id))
+print('\nComplete!')
 
-#eprint = Eprint(URI)
-#print(eprint.body)
+fieldnames = set().union(*(e.keys() for e in eprints))
+with open(sys.argv[1], 'w') as outfile:
+    writer = csv.DictWriter(outfile, fieldnames=fieldnames)
+    writer.writeheader()
+    for e in eprints:
+        for k,v in e.items():
+            e[k] = ' || '.join(v)
+        writer.writerow(e)
